07d0ca368e0f68d58101271089f9d6579c154f28
nikolay-is <niki@vip.bg>
2017-05-31 14:16:12 +0300

    Clean

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  development: {
-    connectionString: 'mongodb://localhost:27017/ShopStopDatabase'
-  },
-  production: {
-    connectionString: 'mongodb://xxx.xxx.xxx.xxx:27017/ShopStopDatabase'
-  }
-}
--- handlers/index.js
+++
@@ -1,7 +0,0 @@
-const homeHandler = require('./home')
-const productHandler = require('./product')
-const categoryHandler = require('./category')
-const staticFilesHandlers = require('./static-files')
-
-module.exports = [ homeHandler, productHandler, categoryHandler, staticFilesHandlers ]
-
